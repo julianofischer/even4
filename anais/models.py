4743b3738afe79bd9d7de9f7d7246a148ca6df37
Juliano Fischer Naves <julianofischer@gmail.com>
2019-04-23 14:54:57 -0400

    descrição dos models

--- anais/models.py
+++ anais/models.py
@@ -2,5 +2,49 @@ from django.db import models
 
 # Create your models here.
 # models: Evento, Trabalho, Autor, Área Temática
+class Evento(models.Model):
+    #titulo
+    #local
+    #data de publicação
+    #apresentação
+    #corpo editorial (comissão científica e comissão organizadora)
+    #areas temáticas
+    #contato
+    #edições anteriores
+    pass
 
+class Anais(models.Model):
+    #titulo
+    #ISBN
+    #data de publicação
+    #país de publicação
+    #idioma de publicação
+    pass
 
+class Trabalho(models.Model):
+    #titulo
+    #autores
+    #modalidade (artigo, banner, ...)
+    #área temática
+    #página do trabalho (URL)
+    #anais
+    #pdf
+    pass
+
+class Autor(models.Model):
+    #nome
+    #nome como citação
+    #contato (e-mail)
+    #afiliação  (universidade)
+    pass
+
+class Afiliacao(models.Model):
+    #nome
+    #sigla
+    #cidade
+    #estado
+    #país
+    pass
+
+class AreaTematica(models.Model):
+    pass
