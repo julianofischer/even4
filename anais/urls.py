5c2f003e79d7ea3df23b74a8d213c65283c50c16
Juliano Naves <julianofischer@gmail.com>
2019-04-26 11:39:25 -0400

    eventos

--- anais/urls.py
+++ anais/urls.py
@@ -4,6 +4,6 @@ from .views import home, event_detail, trabalho_detail
 urlpatterns = [
     path('', home, name='home'),
     path('evento/<int:pk>', event_detail),
-    path('evento/<str:slug>', event_detail ),
+    path('evento/<str:slug>', event_detail, name='eventoslug' ),
     path('trabalhos/<str:slug_evento>/<str:slug_trab>/', trabalho_detail ),
 ]
