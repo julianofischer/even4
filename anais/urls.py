c7830d1cfed3ca9eaf3b7517c064a9d7bc8eaad9
Juliano Fischer Naves <julianofischer@gmail.com>
2019-04-29 22:43:52 -0400

    ok

--- anais/urls.py
+++ anais/urls.py
@@ -4,8 +4,8 @@ from .views import home, event_detail, trabalho_detail, home_ajax_search
 urlpatterns = [
     path('', home, name='home'),
     path('evento/<int:pk>', event_detail),
-    path('evento/<str:slug>', event_detail, name='eventoslug' ),
-    path('trabalhos/<str:slug_evento>/<str:slug_trab>/', trabalho_detail, name='trabalho_detail' ),
+    path('evento/<slug:slug>', event_detail, name='eventoslug' ),
+    path('evento/<slug:slug_evento>/<slug:slug_trab>/', trabalho_detail, name='trabalho_detail' ),
     path('ajax-search/<str:search_string>', home_ajax_search),
     path('ajax-search/', home_ajax_search)
 ]
